2780302610249f19f5b1f4240277d27e6809f886
Vipul Tawde <viptech.ai@gmail.com>
2020-11-14 19:52:58 +0530

    database change to postgre

--- skill_showcase/settings.py
+++ skill_showcase/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'skill_showcase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vipr_database',
+        'USER': 'vipul',
+        'PASSWORD': 'QSXwdcVHUbji',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
